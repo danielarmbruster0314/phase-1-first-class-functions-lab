119982c7ac8225a9fa482520454dc8c279027b52
Daniel Armbruster <danielgundam00@gmail.com>
2022-02-23 16:21:26 -0700

    commit tree fa1c19f2ce1748b76e4a4bd0d6f97ce5b945557c

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (arr) => {
+    return arr.slice(0,2);
+}
+
+const returnLastTwoDrivers = (arr) =>{
+    return arr.slice(arr.length-2,arr.length);
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(quadnum){
+        return (num) => {
+            return quadnum * num;
+        }
+}
+
+const fareDoubler = (func) => {
+    return func * 2;
+}
+
+fareDoubler(createFareMultiplier());
+
+const fareTripler = (func) => {
+    return func * 3;
+}
+
+fareTripler(createFareMultiplier());
+
+function selectDifferentDrivers(arr, func){
+    if(func === returnFirstTwoDrivers){
+        return returnFirstTwoDrivers(arr)
+    }else if(func === returnLastTwoDrivers){
+        return returnLastTwoDrivers(arr)
+    }else{
+        return "sucks to suck!";
+    }
+}
+
